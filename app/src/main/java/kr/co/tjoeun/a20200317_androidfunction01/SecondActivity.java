41a242d7237d9cc320e62163906b7db0a4592f8d
cyhaz <cyhaz@naver.com>
2020-03-17 16:50:08 +0900

    Main에서 데이터 첨부 => second에서 받아서 확인 Log로만 찍어본 상태

--- app/src/main/java/kr/co/tjoeun/a20200317_androidfunction01/SecondActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_androidfunction01/SecondActivity.java
@@ -3,12 +3,20 @@ package kr.co.tjoeun.a20200317_androidfunction01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String sendMessage = getIntent().getStringExtra("message");
+
+        Log.d("받아온String", sendMessage);
+
     }
 }
